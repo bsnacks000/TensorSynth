707a16c9e174799873693f9051cae568e4082f89
bsnacks000 <bsnacks000@gmail.com>
2017-04-16 12:50:07 -0400

    created improv json files for development.. fixed deepCopy issue in grain synth.. fixed binning issues in binning_specs

--- tensor_synth/translate/binning_specs.py
+++ tensor_synth/translate/binning_specs.py
@@ -14,27 +14,27 @@ import re
 
 
 def midi_bins4():
-    return [i for i in range(0,127,32)]
+    return [i for i in range(0,127+32,32)]
 
 
 def midi_labels4():
-    return [string.ascii_lowercase[i] for i in range(3)]
+    return [string.ascii_lowercase[i] for i in range(4)]
 
 
 def midi_bins8():
-    return [i for i in range(0,127,16)]
+    return [i for i in range(0,127+16,16)]
 
 
 def midi_labels8():
-    return [string.ascii_lowercase[i] for i in range(7)]
+    return [string.ascii_lowercase[i] for i in range(8)]
 
 
 def midi_bins16():
-    return [i for i in range(0,127,8)]
+    return [i for i in range(0,127+8,8)]
 
 
 def midi_labels16():
-    return [string.ascii_lowercase[i] for i in range(15)]
+    return [string.ascii_lowercase[i] for i in range(16)]
     
 
 def dur_bins12():
